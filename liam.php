de3d9447d5b697ccb2c8c8a940ee4b79b2519c4d
drkameleon <yaniszaf@gmail.com>
2022-09-08 11:18:53 +0200

    update

--- liam.php
+++ liam.php
@@ -7,38 +7,38 @@ use PHPMailer\PHPMailer\Exception;
 
 if (isset($_POST["validated"])) {
 
-    require '../utils/vendor/autoload.php';
+    // require '../utils/vendor/autoload.php';
 
-    $mail = new PHPMailer(true);
+    // $mail = new PHPMailer(true);
 
-    try {
+    // try {
 
-        $website = 'getyummy.app';
-        $message = nl2br($_POST['message']);
+    //     $website = 'getyummy.app';
+    //     $message = nl2br($_POST['message']);
         
-        $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+    //     $mail->SMTPDebug = SMTP::DEBUG_SERVER;
 
-        $mail->isSMTP();                                         
-        $mail->Host       = "smtp.zoho.eu";                    
-        $mail->SMTPAuth   = true;                                
-        $mail->Username   = 'info@ensili.co';
-        $mail->Password   = 'QQY0Rz25iAFR';
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-        $mail->Port       = 587;
+    //     $mail->isSMTP();                                         
+    //     $mail->Host       = "smtp.zoho.eu";                    
+    //     $mail->SMTPAuth   = true;                                
+    //     $mail->Username   = 'info@ensili.co';
+    //     $mail->Password   = 'QQY0Rz25iAFR';
+    //     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    //     $mail->Port       = 587;
 
-        $mail->setFrom("info@ensili.co", "SUPPORT");
-        $mail->addReplyTo($_POST['email'], $_POST['name']);
-        $mail->addAddress('info@ensili.co', 'enSili.co');
+    //     $mail->setFrom("info@ensili.co", "SUPPORT");
+    //     $mail->addReplyTo($_POST['email'], $_POST['name']);
+    //     $mail->addAddress('info@ensili.co', 'enSili.co');
 
-        $mail->isHTML(true);
-        $mail->Subject = "[SUPPORT] ". $_POST['subject'];
-        $mail->Body    = "<b>Website:</b><br>{$website}<br><br><b>Subject:</b><br>{$_POST['subject']}<br><br><b>Message:</b><br>{$message}";
+    //     $mail->isHTML(true);
+    //     $mail->Subject = "[SUPPORT] ". $_POST['subject'];
+    //     $mail->Body    = "<b>Website:</b><br>{$website}<br><br><b>Subject:</b><br>{$_POST['subject']}<br><br><b>Message:</b><br>{$message}";
 
-        $mail->send();
-        echo 'OK';
-    } catch (Exception $e) {
-        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-    }
+    //     $mail->send();
+    //     echo 'OK';
+    // } catch (Exception $e) {
+    //     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    // }
 }
 else {
     echo 'BLOCKED';
